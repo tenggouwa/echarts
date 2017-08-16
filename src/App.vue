d85932f4a2da8ebff41dc390dad108b67ad42faa
tenggouwa <tenggouwa>
2017-08-16 12:13:22 +0800

    000

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
+    <Hello></Hello>
+    <!-- <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
@@ -15,17 +16,21 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
+import Hello from './hello.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components:{
+    Hello
   }
 }
 </script>
--- src/hello.vue
+++ src/hello.vue
@@ -1,13 +1,73 @@
 <template>
-	<div id="echarts1">
-		<div class="myChart" :style="{width:'300px',height:'300px'}"></div>
+	<div id="main" :style="{width:'500px',height:'600px'}">
+		
 	</div>
 </template>
 <script>
-	let echarts = require('echarts/lib/echarts');
-	require('echarts/lib/chart/bar');
-	require('echarts/lib/component/tooltip');
-	require('echarts/lib/component/toolbox');
-	require('echarts/lib/component/legend');
-	require('echarts/lib/component/markLine');
+	import echarts from 'echarts'
+	export default{
+		name:'myChart',
+		data(){
+			return{
+				charts:'',
+				opinion:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+				opinionData:[
+                    {value:335,name:'直接访问'},
+                    {value:310,name:'邮件营销'},
+                    {value:234,name:'联盟广告'},
+                    {value:135,name:'视频广告'},
+                    {value:1548,name:'搜索引擎'},
+				]
+			}
+		},
+	    methods:{
+            drawPie(id){
+               this.charts = echarts.init(document.getElementById(id))
+               this.charts.setOption({
+                 tooltip: {
+                   trigger: 'item',
+                   formatter: '{a}<br/>{b}:{c} ({d}%)'
+                 },
+                 legend: {
+                   orient: 'vertical',
+                   x: 'left',
+                   data:this.opinion
+                 },
+                 series: [
+                   {
+                     name:'访问来源',
+                     type:'pie',
+                     radius:['50%','70%'],
+                     avoidLabelOverlap: false,
+                     label: {
+                       normal: {
+                         show: false,
+                         position: 'center'
+                       },
+                       emphasis: {
+                         show: true,
+                         textStyle: {
+                           fontSize: '30',
+                           fontWeight: 'blod'
+                         }
+                       }
+                     },
+                     labelLine: {
+                       normal: {
+                         show: false
+                       }
+                     },
+                     data:this.opinionData
+                   }
+                 ]
+               })
+            }
+        },
+      //调用
+        mounted(){
+            this.$nextTick(function() {
+                this.drawPie('main')
+            })
+        }
+    }
 </script>
